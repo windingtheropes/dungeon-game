b0b9b818566cc63b2fc5d669b6c6bc32d326b5ed
Jack Anderson <jack@Jacks-MacBook-Air.local>
2024-10-26 19:19:45 -0400

    add screen base library, blogger base library

---
+++ blogger.py
@@ -0,0 +1,27 @@
+# logging library
+from datetime import datetime
+class blogger():
+    def __init__(self, file=f"log"):
+        ds = datetime.now().strftime("%m_%d_%Y__%H_%M_%S")
+        self.file = f"{file}_{ds}"
+    def _log(self, p,m):
+        ts = datetime.now().strftime("%H:%M.%S")
+        msg = f"[{ts}][{p}] {m}"
+        print(msg)
+        buf = open(self.file, "+a")
+        buf.write(f"{msg}\n")
+        buf.close()
+    def warn(self, m):
+        self._log("warn", m)
+    def error(self, m):
+        self._log("error", m)
+    def info(self, m):
+        self._log("info", m)
+
+# blogger init function to be used globally, all log to one file
+b: blogger = None
+def blog():
+    return b
+def init(file):
+    global b
+    b = blogger(file)
---
+++ game.py
@@ -0,0 +1,57 @@
+import pygame
+import blogger
+from screen import Screen
+
+# initialize blogger for global use
+blogger.init("log/file")
+blog = blogger.blog()
+
+pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode([500,500])
+
+# level 1
+class Game():
+    def __init__(self):
+        self.running = True
+        self.active_screen = -1
+        self.screens = []
+        pass
+    def start(self):
+        # start render loop
+        while self.running == True:
+            if(self.active_screen == -1):
+                blog.error("No running screen. Abort.")
+                break
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+            
+            # render the active screen
+            for s in self.screens:
+                if(s.active == True):
+                    s.render()
+
+            # refresh the screen 1/24 of a second for 24fps
+            pygame.display.flip()
+            clock.tick(24)
+    # register a screen to the screens table
+    def addScreen(self, screen: Screen):
+        if screen in self.screens:
+            return
+        else:
+            self.screens.append(screen)
+
+class newscreen(Screen):
+    def __init__(self):
+        Screen.__init__(self)
+        super(newscreen, self)._listen("render", self.render)
+    def render():
+        print("hello")
+
+g = Game()
+g.addScreen(newscreen())
+g.start()
+
+
