af7b243af900ab7dfc32464ffe648a75e6b76249
Jack Anderson <jack.dev@alacriware.com>
2024-11-07 19:55:06 -0500

    add unit vector and magnitude methods

--- vector.py
+++ vector.py
@@ -1,10 +1,21 @@
+import math
 class Vec2():
-    def __init__(self, x=0, y=0):
-        self.x = x
-        self.y = y
+    def __init__(self, x:float=0, y:float=0):
+        self.x:float = float(x)
+        self.y:float = float(y)
         self.arr = [self.x,self.y]
+    # magnitude of vector; use is to be interpreted
+    def mag(self):
+        return math.sqrt(self.x**2 + self.y**2)
+    # return the corresponding unit vector
+    def unit(self):
+        return (1/self.mag()) * self
+    # multiplication for int and float, scalar multiplication of vector
     def __mul__(self,b):
         return Vec2(self.x*b, self.y*b)
+    def __rmul__(self,b):
+        return Vec2(self.x*b, self.y*b)
+    # addition of two vectors adds components
     def __add__(self, b):
         if(isinstance(b, Vec2)):
             return Vec2(self.x + b.x, self.y + b.y)
