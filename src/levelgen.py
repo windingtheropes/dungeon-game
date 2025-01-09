972866f970e8ec9f5ec3491db8cea68e70db34d5
windingtheropes <jack.dev@alacriware.com>
2025-01-08 22:36:22 -0500

    improve level generation algorithm; first implementation of making sure that player can access all enemies and vise versa

--- src/levelgen.py
+++ src/levelgen.py
@@ -35,17 +35,17 @@ def chance(c): # 1 in c
     if(random.randint(1,c) == random.randint(1,c)):
         return True
     return False
-# find if a coordinate (cell) has openings next to it
-def getOpenings(coord, level):
+# find if a coordinate (cell) has openings next to it, accept dict of other values to return as well
+def getOpeningsOnCell(coord, level, dict=[0]):
     openings = []
     adj = adjacent_coords(coord[0], coord[1])
     for a in adj:
         r = a[0]
         c = a[1]
-        if level[r][c] == 0:
+        if level[r][c] in dict:
             openings.append([r,c])
     return openings
-def getEmptyCells(level):
+def getEmptyCellsOnLevel(level):
     coords = []
     for r in range(0, len(level)):
         for c in range(0,len(level[r])):
@@ -60,15 +60,40 @@ def emptycount(level):
             if c == 0:
                 count+=1
     return count
-# of a list of spaces, only return those that have openings on x (thresh) sides
+
+# recursively find all cells in a 'cave' by checking for openings, and the player
+def getAllAccessible(coord, level, counted=[]):
+    openings = getOpeningsOnCell(coord, level, [0,"p"])
+    for o in openings:
+        if not o in counted:
+            counted.append(o)
+            return getAllAccessible(o, level, counted)
+    return counted
+# array of coords to array of values 
+def toValue(arr, level):
+    mapped = []
+    for val in arr:
+        mapped.append(level[val[0]][val[1]])
+    return mapped
+
+# of a list of spaces, only return one that has thresh amount of openings
 def getWideSpace(spaces, level, thresh=2):
     spaces: list
     s = spaces[random.randint(0, len(spaces)-1)]
-    if(len(getOpenings(s, level)) >= thresh):
+    if(len(getOpeningsOnCell(s, level)) >= thresh):
         return s
     else:
         spaces.remove(s)
         return getWideSpace(spaces, level)
+    
+# only return spaces that are accessible to the player
+def getPlayerAccessibleSpace(level, openSpaces):
+    s = getWideSpace(openSpaces, level)
+    # print(toValue(getAllAccessible(s, level), level))
+    if not "p" in toValue(getAllAccessible(s, level), level):
+        openSpaces.remove(s)
+        return getPlayerAccessibleSpace(level, openSpaces)
+    return s
 
 # generate a level, procedurally
 def gen():
@@ -95,22 +120,23 @@ def gen():
                     if(chance(2)):
                         level[r][c] = 1;
                     continue
-
-    openSpaces = getEmptyCells(level)
+    
+    openSpaces = getEmptyCellsOnLevel(level)
+    s = getWideSpace(openSpaces, level)
+    level[s[0]][s[1]] = "p"
+    for i in openSpaces:
+        print(toValue(getAllAccessible(i, level), level))
+    openSpaces = getEmptyCellsOnLevel(level)
     for i in range(0, random.randint(0,6)):
-        s = getWideSpace(openSpaces, level)
+        s = getPlayerAccessibleSpace(level, openSpaces)
         level[s[0]][s[1]] = "3"
 
-    openSpaces = getEmptyCells(level)
+    openSpaces = getEmptyCellsOnLevel(level)
     for i in range(0, random.randint(2,4)):
-        s = getWideSpace(openSpaces, level)
+        s = getPlayerAccessibleSpace(level, openSpaces)
         level[s[0]][s[1]] = "2"
 
-    openSpaces = getEmptyCells(level)
-    s = openSpaces[random.randint(0, len(openSpaces)-1)]
-    level[s[0]][s[1]] = "p"
-
-    # level[5][5] = "p"
+    
     stringform = ""
     for r in range(0,len(level)):
         for c in range(0,len(level[r])):
